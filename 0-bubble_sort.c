6b22f8685d0f17c7609f0fc09350a82419646917
Diegoacosta127 <diegoacosta127@gmail.com>
2022-02-10 12:15:29 -0800

    first commit -leave me alone-

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+  * bubble_sort - sorts an array of integers in ascending order
+  * @array: pointer to array to sort
+  * @size: size of the array
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, flag;
+	int aux;
+
+	do {
+		flag = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				aux = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = aux;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+	} while (flag == 1);
+}
